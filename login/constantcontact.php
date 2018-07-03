2e667e54013793a437e86cf8af1eb6c9d98456de
Ajay Arivu <arivazhagan.pandi@arkinfotec.com>
2018-07-03 11:23:18 +0530

    #Day 4 Commit

---
+++ login/constantcontact.php
@@ -0,0 +1,19 @@
+<form id="frm-cc" action="http://localhost/contacts/offer.php">
+    <input type="hidden" name="type"  value="constantcontact"/>
+    <input type="hidden" name="access_token" id="cc_access_token" value=""/> 
+</form>
+<script type="text/javascript">
+    var url = location.hash;
+    if (url.indexOf("#access_token") > -1) {
+        var new_url = url.replace("#", "");
+        console.log(new_url);
+        var segments = new_url.split("&");
+        var access_token = "";
+        console.log(segments);
+        if (segments.length > 0) {
+            access_token = segments[0].split("=");
+        }
+        document.getElementById("cc_access_token").value = access_token[1];
+        document.getElementById("frm-cc").submit();
+    }
+</script>
